89ed83de859a7034bfc2b6f4614eb5c6415e9a66
Sudeep Prasad <sudeep.rp@gmail.com>
2025-07-21 10:41:16 +0530

    stronger tshirt tests

--- tshirts.py
+++ tshirts.py
@@ -8,7 +8,9 @@ def size(cms):
         return 'L'
 
 
+assert(size(5) == 'Error')
 assert(size(37) == 'S')
+assert(size(38) == 'S')
 assert(size(40) == 'M')
 assert(size(43) == 'L')
 print("All is well (maybe!)")
